cc370537cb15582030d39f8e192510cb0ca250a4
IMRAN ALI <imran30599>
2024-08-26 01:20:15 +0530

    Add more files

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ['Web Developer.', 'Frontend Developer.' , 'Backend Developer.'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
